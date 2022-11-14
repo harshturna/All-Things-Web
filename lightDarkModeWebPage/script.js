fe428a031b62a30c4d19f6b250a4d7eeb7c70fa7
Harsh <harshturna15@gmail.com>
2022-11-13 19:37:15 -0600

    add Local Storage to save the current theme

--- lightDarkModeWebPage/script.js
+++ lightDarkModeWebPage/script.js
@@ -30,7 +30,7 @@ function lightMode() {
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
 
-    imageMode('light')
+    imageMode('light');
 
 }
 
@@ -38,10 +38,12 @@ function lightMode() {
 const switchTheme = (event) => {
     if(event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 
@@ -49,3 +51,14 @@ const switchTheme = (event) => {
 
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
